46f47849c6fd0de0c83a76c93597abe7cf067535
po394026 <po394026@ucf.edu>
2024-08-19 21:36:15 -0400

    Add files via upload

---
+++ Main.R
@@ -0,0 +1,11 @@
+##############################
+###     Main.R             ###
+##############################
+
+rm(list = ls())
+source("Functions.R")
+
+x <- 3
+y <- 4
+z <- AddArguments(x,y)
+print(z)
